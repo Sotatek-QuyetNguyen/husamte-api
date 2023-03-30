8578a044916cd321636b3349cb7e890804e1f9ac
Nathan <hung.dinh2@sotatek.com>
2023-03-30 18:27:51 +0700

    Add feature update property profile

--- src/modules/gateway/property/property.controller.ts
+++ src/modules/gateway/property/property.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Param, Post, Put } from "@nestjs/common";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
-import { CreatePropertyDTO } from "./property.dto";
+import { CreatePropertyDTO, UpdatePropertyDTO } from "./property.dto";
 import { PropertyService } from "./property.service";
 
 
@@ -15,5 +15,13 @@ export class PropertyController {
     @Body() body: CreatePropertyDTO
   ): Promise<any> {
     return await this.propertyService.createProperty(body);
+  }
+
+  @Put()
+  @ApiOperation({ summary: `Update property profile` })
+  async update(
+    @Body() body: UpdatePropertyDTO
+  ): Promise<any> {
+    return await this.propertyService.updateProperty(body);
   }
 }
--- src/modules/gateway/property/property.dto.ts
+++ src/modules/gateway/property/property.dto.ts
@@ -1,7 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { ArrayMaxSize, ArrayMinSize, IsArray, IsInt, IsNotEmpty, IsNumber, IsNumberString, Matches, registerDecorator, ValidateNested, ValidationArguments, ValidationOptions } from "class-validator";
+import { ArrayMaxSize, ArrayMinSize, IsArray, IsBoolean, IsIn, IsInt, IsNotEmpty, IsNumber, IsNumberString, IsOptional, IsString, Matches, Max, MaxLength, Min, registerDecorator, ValidateNested, ValidationArguments, ValidationOptions } from "class-validator";
 import { IsBiggerThanZero } from "src/share/common/base.dto";
+import { FIELD_REQUIRED, REGEX_ONLY_NUMBER_MAX_2_AFTER_DOT } from "src/share/common/constants";
+import { AIR_CONDITIONING_TYPE, PROPERTY_BASEMENT, PROPERTY_EXTERNAL, PROPERTY_TYPE, ROOM_FEATURE, ROOM_TYPE } from "./property.const";
 
 export function IsBiggerThanZeroAndSmallerThan100(
   property: any,
@@ -17,6 +19,31 @@ export function IsBiggerThanZeroAndSmallerThan100(
       validator: {
         validate(value: any, args: ValidationArguments) {
           return value > 0 && value < 100;
+        },
+      },
+    });
+  };
+}
+
+export function IsCorrectRoomFeature(
+  property: any,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'IsCorrectRoomFeature',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [property],
+      options: validationOptions,
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          for (const num of value) {
+            if (!Object.values(ROOM_FEATURE).includes(num)) {
+              return false;
+            }
+          }
+          return true;
         },
       },
     });
@@ -64,4 +91,167 @@ export class CreatePropertyDTO {
   @Type(() => ownerDTO)
   @ValidateNested({ each: true })
   owners: ownerDTO[];
+}
+
+export class UpdatePropertyDTO {
+  @ApiProperty({
+    description: "Id of property",
+    required: true,
+    example: 1,
+  })
+  @IsNotEmpty()
+  @IsInt({ message: 'Input can only contain number.' })
+  @IsBiggerThanZero('id', {
+    message: 'Value must be higher than 0.',
+  })
+  @Type(() => Number)
+  id: number;
+
+  @ApiProperty({
+    description: "Name of property",
+    example: "EcoCity",
+  })
+  @IsString()
+  @IsNotEmpty({ message: FIELD_REQUIRED })
+  @MaxLength(100, { message: 'Maximum 100 characters.' })
+  name: string;
+
+  @ApiProperty({
+    description: "Description",
+    example: "Description",
+  })
+  @IsString()
+  @IsNotEmpty({ message: FIELD_REQUIRED })
+  @MaxLength(1000, { message: 'Maximum 1000 characters' })
+  description: string;
+
+  @ApiProperty({
+    description: "Image url",
+    example: "https://image.com",
+  })
+  @IsNotEmpty()
+  @IsString()
+  image: string;
+
+  @ApiProperty({
+    description: "Type of property",
+    example: 1,
+  })
+  @IsInt()
+  @IsNotEmpty()
+  @IsIn(Object.values(PROPERTY_TYPE))
+  type: number;
+
+  @ApiProperty({
+    description: "Property's acreage, unit is ft",
+    example: 120.5,
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  acreage: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0)
+  @Max(20)
+  bedroomCount: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0)
+  @Max(20)
+  bathroomCount: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0)
+  parkingSlot: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsIn(Object.values(AIR_CONDITIONING_TYPE))
+  airConditioningType: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsIn(Object.values(PROPERTY_EXTERNAL))
+  external: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsIn(Object.values(PROPERTY_BASEMENT))
+  basement: number;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  hasKitchen: boolean;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  hasFoyer: boolean;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  hasBreakfast: boolean;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  hasLivingroom: boolean;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  hasDiningroom: boolean;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  hasFamilyroom: boolean;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  hasLaundry: boolean;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(1000, { message: 'Input’s maximum length is 1000 characters.' })
+  extras: string;
+
+  @ApiProperty({
+    description: "Room detail of property"
+  })
+  @IsOptional()
+  @IsArray()
+  @Type(() => RoomDTO)
+  @ValidateNested({ each: true })
+  rooms: RoomDTO[]
+}
+
+export class RoomDTO {
+  @ApiProperty({
+    description: "Type of room",
+    example: ROOM_TYPE.BEDROOM,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  type: number;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  @Matches(REGEX_ONLY_NUMBER_MAX_2_AFTER_DOT, {
+    message: 'Length must be a number and have max 2 digits after decimal'
+  })
+  length: number;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  @Matches(REGEX_ONLY_NUMBER_MAX_2_AFTER_DOT, {
+    message: 'Width must be a number and have max 2 digits after decimal'
+  })
+  width: number;
+
+  @IsArray()
+  @IsOptional()
+  @IsCorrectRoomFeature('feature', {
+    message: 'Room feature invalid'
+  })
+  feature: number[];
 }
